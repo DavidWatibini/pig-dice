f8f9ba5f6db2d527fdeaf4fd6d1d4b56be380d59
Watinguyaz <watimakhanu@gmail.com>
2018-12-17 14:23:18 +0300

    setup for players prototypes and constructs finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,39 @@
-function gameOn("","") {
+function gameOn("", "") {
   this.player1 = "";
   this.player2 = "";
 }
-var diceRoll= function () {
-  return Math.floor(Math.random() * 6)+1;
+
+function Players(swichPlayer) {
+  this.diceRoll = 0;
+  this.roundTotal = 0;
+  this.totalScore = 0;
+  this.swichPlayer = swichPlayer;
+  this.playerName;
+}
+var diceRoll = function() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+Players.prototype.RollingOne = function() {
+  if (this.diceRoll === 1) {
+    this.totalScore = 0;
+    this.roundTotal = 0;
+    this.diceRoll=0;
+    alert("oh! no " + this.playerName + ", you just rolled a 1!, allow next player to play ")
+  } else {
+    this.roundTotal += this.diceRoll;
+  }
+}
+
+Players.prototype.hold = function () {
+  this.totalScore += this.roundTotal;
+  this.roundTotal = 0;
+  this.diceRoll=0;
+}
+
+Players.prototype.newGame = function () {
+    location.reload();
+  }
 
 //user interface //
 
