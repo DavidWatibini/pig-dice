f156a5d1f716ac58a482101f1dbb91a2581b9872
Watinguyaz <watimakhanu@gmail.com>
2018-12-17 08:14:13 +0300

    hide and show attributes done

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#button-roll").click(function() {
+    var numbers = [1, 2, 3, 4, 5, 6]
+    var randomSum = 0;
+    var randomNumber = Math.floor(Math.random() * (numbers.length));
+    if(randomNumber===1) {
+      randomSum=0;
+      alert("You just rolled a 1. pass dice to next player")
+    } else if (randomNumber===2) {
+      randomSum+=2;
+
+    }
+    $('#roll').text(numbers[randomNumber]);
+    $('#toroll').text(numbers[randomNumber]);
+  });
+});
+
+//user interface //
+
+$(document).ready(function() {
+  $("#play").click(function() {
+    $(".content").show();
+    $("#new-player").hide();
+    $("#play").hide();
+  });
+});
